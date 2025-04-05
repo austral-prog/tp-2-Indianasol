fd46fc42b179826769327dfdb013b3d081c026ca
Indianasol <ideirmendjian@mail.austral.edu.ar>
2025-04-05 19:49:13 -0400

    Update earth.py

--- earth.py
+++ earth.py
@@ -4,4 +4,4 @@ def earth():
     resultado1 = x < y
     resultado2 = x > y
     print(f"The result of {x} comes first in the dictionary than {y} is {resultado1}.")
-    print(f"The result of {y} comes first in the dictionary than {x} is {resultado2}."
+    print(f"The result of {y} comes first in the dictionary than {x} is {resultado2}.")
